ca3465e491c6792e92920cc399a53e7c6bbb4247
fififika <111443160+fififika@users.noreply.github.com>
2022-11-07 15:13:42 +0700

    Create Tipe bilangan.py

---
+++ Tipe bilangan.py
@@ -0,0 +1,24 @@
+# integer
+tipe_data = 10
+print("data = ", tipe_data) # menampilkan nilai variabel pada terminal
+print("tipe data = ", type(tipe_data)) #menampilkan tipe data yang digunakan
+
+#  float
+tipe_data = 1.7
+print("data = ", tipe_data) # menampilkan nilai variabel pada terminal
+print("tipe data = ", type(tipe_data)) #menampilkan tipe data yang digunakan
+
+# string
+tipe_data = "universitas negeri padang"
+print("data = ", tipe_data) # menampilkan nilai variabel pada terminal
+print("tipe data = ", type(tipe_data)) #menampilkan tipe data yang digunakan
+
+# boolean
+tipe_data = True
+print("data = ", tipe_data) # menampilkan nilai variabel pada terminal
+print("tipe data = ", type(tipe_data)) #menampilkan tipe data yang digunakan
+
+# bilangan kompleks
+tipe_data = complex(2,6)
+print("data = ", tipe_data) # menampilkan nilai variabel pada terminal
+print("tipe data = ", type(tipe_data)) #menampilkan tipe data yang digunakan
