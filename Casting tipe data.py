bedac867849c60900e40f9d6410186973d2f8065
fififika <111443160+fififika@users.noreply.github.com>
2022-11-07 15:52:28 +0700

    Create Casting tipe data.py

---
+++ Casting tipe data.py
@@ -0,0 +1,15 @@
+# var = tipe_data(var)
+
+data = 12.4 # data float
+print("data = ", data)
+print("tipe data = ", type(data)) 
+
+data_1 = int(data)
+print("data hasil casting = ", data_1) 
+print("tipe data hasil casting = ", type(data_1)) 
+data_2 = str(data)
+print("data hasil casting = ", data_2) 
+print("tipe data hasil casting = ", type(data_2)) 
+data_3 = bool(data)
+print("data hasil casting = ", data_3) 
+print("tipe data hasil casting = ", type(data_3)) 
